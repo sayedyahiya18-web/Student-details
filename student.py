0bee14eafecf4795f98a427d9eb814781306e477
SayedMohammedYahiyaKhadri <sayedyahiya18@gmail.com>
2025-11-10 11:41:49 +0530

    Implement default values for student info
    Added default values for name and roll number if no input is provided.

---
+++ student.py
@@ -0,0 +1,15 @@
+import sys
+if len(sys.argv) !=3:
+  script_name = sys.argv[0]
+  name = sys.argv[1]
+  rollno = sys.argv[2]
+else:
+  script_name = sys.argv[0]
+  name = "sayed"
+  rollno = "067"
+  print("No input given - using default values:")
+
+print("Script name:",script_name)
+print("Student name:",name)
+print("Roll number:",rollno)
+
